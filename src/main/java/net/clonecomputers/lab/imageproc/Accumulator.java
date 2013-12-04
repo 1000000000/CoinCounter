8d9bde025019eb6e48ba34a37c887b755705f42f
1000000000 <lthyde@gmail.com>
2013-12-04 15:26:16 -0500

    Added throws IllegalAccessException to Accumulator.threshold()

--- src/main/java/net/clonecomputers/lab/imageproc/Accumulator.java
+++ src/main/java/net/clonecomputers/lab/imageproc/Accumulator.java
@@ -5,6 +5,6 @@ import java.util.Set;
 public interface Accumulator<T> {
 
 	public void accumulate();
-	public Set<T> threshold(double thresh);
+	public Set<T> threshold(double thresh) throws IllegalAccessException;
 	
 }
